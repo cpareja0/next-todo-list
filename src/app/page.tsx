1142f3b9fd801e56caefb421a6d38491101de958
Calvin Pareja <capareja1104@gmail.com>
2023-09-28 09:00:01 +0800

    added crossout feature and completed new todo request

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,12 @@ function getTodos() {
   return prisma.todo.findMany();
 }
 
+async function toggleTodo(id: string, complete: boolean) {
+  "use server";
+
+  console.log(id, complete);
+}
+
 export default async function Home() {
   const todos = await getTodos();
   //Test todo item
@@ -24,7 +30,7 @@ export default async function Home() {
       </header>
       <ul className="pl-4">
         {todos.map((todo) => (
-          <TodoItem key={todo.id} {...todo} />
+          <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo} />
         ))}
       </ul>
     </>
